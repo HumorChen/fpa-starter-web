8a96de8e75745c7dc77527366db4bbac5f272da8
陈福星 <3301633914@qq.com>
2021-12-08 00:52:00 +0800

    1. 修复dubbo provider异常传递到consumer被封装到RuntimeException问题 2. 加入参数校验器 springboot validation 3. 加入全局请求参数异常拦截器

--- src/main/java/cn/freeprogramming/config/GlobalExceptionResolver.java
+++ src/main/java/cn/freeprogramming/config/GlobalExceptionResolver.java
@@ -4,12 +4,18 @@ package cn.freeprogramming.config;
 import cn.freeprogramming.enums.CommonErrorEnums;
 import cn.freeprogramming.error.BusinessError;
 import cn.freeprogramming.vo.result.R;
+import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * 全局Controller层异常处理类
@@ -32,23 +38,39 @@ public class GlobalExceptionResolver {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public R handleException(Exception e) {
-        // 打印异常堆栈信息
-        //dubbo异常处理
-        if (e instanceof RuntimeException )
+        log.error(e.getMessage());
         log.error("异常拦截器打印");
         log.error("类名："+e.getClass().getName());
         log.error("错误信息："+e.getMessage());
         log.error("打印整个错误："+e);
-        try {
-            BusinessError businessError = (BusinessError)e;
-            if (businessError != null){
-                return R.error(businessError);
-            }
-        } catch (Exception exception) {
-            System.err.println("转化失败");
-            exception.printStackTrace();
-        }
         return R.error(CommonErrorEnums.BUSINESS_ERROR);
+    }
+
+    /**
+     * 缺少请求体或者请求体不可读
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseBody
+    public R handleRequestBodyException(HttpMessageNotReadableException e){
+        log.error(e.getMessage());
+        return R.error(CommonErrorEnums.ILLEGAL_REQUEST_BODY);
+    }
+
+    /**
+     * 参数校验不通过
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public R handleNotValidArgumentException(MethodArgumentNotValidException e){
+        log.error(e.getMessage());
+        List<ObjectError> allErrors = e.getBindingResult().getAllErrors();
+        Set<String> tips = new HashSet<>(allErrors.size());
+        allErrors.forEach(p->tips.add(p.getDefaultMessage()));
+        return new R(CommonErrorEnums.ILLEGAL_ARGUMENT.name(),"参数审核未通过",tips);
     }
 
     /**
@@ -60,8 +82,7 @@ public class GlobalExceptionResolver {
     @ExceptionHandler(BusinessError.class)
     @ResponseBody
     public R handleOpdRuntimeException(BusinessError e) {
-        // 不打印异常堆栈信息
-        log.error(e.getMessage());
+        log.error(e.getErrorMessage());
         return R.error(e);
     }
 }
